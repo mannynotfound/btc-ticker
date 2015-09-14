f8242249b5646a707172371c8096dfca230b2ab7
Cameron Moberg <cjmoberg@gmail.com>
2015-09-14 00:15:51 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+try:
+            from setuptools import setup
+except ImportError:
+            from distutils.core import setup
+
+setup(name='clockr',
+      description='Simple curses clock written in Python',
+      long_description=open('README.md').read(),
+      version='0.8',
+      author='Johnathan "ShaggyTwoDope" Jenkins',
+      author_email='twodopeshaggy@gmail.com',
+      url='https://github.com/shaggytwodope/clockr',
+      data_files=[
+      ("share/man/man1", ["clockr.1"]),
+      ("share/doc/clockr", ["LICENSE"])
+      ],
+      scripts=['clockr'],
+      classifiers=['Intended Audience :: End Users/Desktop',
+                   'Programming Language :: Python :: 3'],
+      license='MIT')
